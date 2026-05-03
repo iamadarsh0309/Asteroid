e803626fb25b1d6c15b0a9720e63739b7d59d3ab
Adarsh Pandey <adarshpandeyy00@gmail.com>
2026-05-03 23:12:22 +0530

    Draw player(triangle) but represent as a circle for collisions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
@@ -10,16 +12,23 @@ def main():
 
     clk = pygame.time.Clock()
     dt =  0 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         delta_time = clk.tick(60)
         dt = delta_time / 1000.0
         # print(dt)
+        
+
+    
 
 
 if __name__ == "__main__":
